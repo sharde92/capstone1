b9fa9f1b83361e17d38ec60fc12f4590ff4c4713
sharde92 <40610538+sharde92@users.noreply.github.com>
2018-07-22 23:11:59 -0500

    Update Capstone1.java

--- src/Capstone1.java
+++ src/Capstone1.java
@@ -1,78 +1,59 @@
-import java.util.Scanner;
+package capstone1;
+ import java.io.*;
+ import java.util.Scanner;
+public class PigLatin1 {
 
-public class Capstone1 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        
-        String input;
+        String englishWord,pigLatinWord;
+        char userInput;
     
-        String output;
-        // Do While Example: Always runs the first time, then checks after.
+        
+        boolean response;
         do {
-            // While Loop Example: Check before running the first time.
-            System.out.println("What word would you like to translate into Pig Latin? ");
-            input = scnr.nextLine();
-            
-            
-            
-            
-            
-            //Put to lower case
-            input.toLowerCase();
-            input=input.toLowerCase();
-            
-            
-            
-            
-            
-            //find where vowels are
-            System.out.println(input.contains("i"));
-            System.out.println(input.indexOf("i"));
-            System.out.println(input.startsWith("i"));
-            
-            
-            
-            
-            
-            //Make sure word starts with vowel
-            //if starts with vowel add -way to end
-            //Use String.startsWith
-            
-            
-            
-            
-            //else if word is consonant, move all consonants 
-            //before first vowel to end of word and add "ay
-            
-            
-            
-            //print word
-            System.out.println(input);
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            System.out.println("Do you want to play again? (y/n)");
-            input = scnr.nextLine();
-        } while (input.equals("y"));
+        System.out.println("Enter a word: ");
+        englishWord = scnr.next();
+        pigLatinWord = convert(englishWord);
+        System.out.println("The result is: " + pigLatinWord );
         
-        System.out.println("Goodbye.");
-        
-
+        System.out.println("Would you like to enter another word? (y/n)");
+        userInput = scnr.next().charAt(0);
+        if (userInput =='y') {
+            response = true;
+        }else {response = false;
+        System.out.println("Goodbye! ");
+        }
+        }while (response == true);
         scnr.close();
-    } 
+}
+    public static String convert (String s)
+    {
+    String consonants,firstVowel,finalWord;
+    int l, i;
+    char letter;
+    s = s.toLowerCase();
+    l = s.length();
     
-    //Find the first vowel of a word
-    //The return 
-    private static int findVowel (String [] vowels) {
-    	
+    
+    for(i = 0; i < l; i++) {
+    
+        letter = s.charAt(i);
+        if (letter=='a'|| letter=='e'|| letter=='i'|| letter=='o'||letter=='u') {
+            break;
+        }
     }
     
+    if (i == 0) {
+        finalWord = s + "way";
+    }
+    
+    else {
+    consonants = s.substring(0,i);
+    firstVowel = s.substring(i);
+    finalWord = firstVowel + consonants + "ay";
+    }
+    
+    return finalWord;
 }
-
+}
